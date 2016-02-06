02c9cdd8be9eb8270f83a1abca66d9095bd99d0f
Aaron Paul <kalistibot@yahoo.com>
2016-02-05 20:47:45 -0600

    Initial Commit

---
+++ LinkedListImplementation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedListImplementation
+//
+//  Created by Aaron on 2/5/16.
+//  Copyright © 2016 Aaron. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
